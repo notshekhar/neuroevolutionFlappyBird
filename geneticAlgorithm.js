686eb1dedc875d4e97c602a9c240a12689222116
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-05-20 09:35:31 +0530

    Update geneticAlgorithm.js

--- geneticAlgorithm.js
+++ geneticAlgorithm.js
@@ -1,65 +1,33 @@
-let genes = null
-function nextGeneration(canvas, total, birds, size, color, diedBirds){
-  // console.log('died', diedBirds)
-  let fittest
-  let n = -1
-  if(genes){
-    fittest = tenFittest(diedBirds, genes)
-    genes = fittest
-    // console.log(genes)
-  }else{
-    fittest = tenFittest(diedBirds)
-    genes = fittest
+function nextGeneration(canvas) {
+  console.log('next generation');
+  calculateFitness();
+  for (let i = 0; i < total; i++) {
+    birds[i] = pickOne();
   }
-  //reprodusing new population
-  for(let i=0; i<total/10; i++){
-    let mutationRate = Math.random()*0.99
-    for(let i=0; i<fittest.length; i++){
-      let brain = fittest[i].brain.copy()
-      let childBird
-      if(i == 0){
-        childBird = new bird(canvas, 20, 'rgba(0, 0, 255, 0.4)', brain)
-      }else{
-        childBird = new bird(canvas, 20, 'rgba(255, 255, 255, 0.4)', brain)
-      }
-      childBird.mutate(mutationRate)
-      birds.push(childBird)
-      n++
-    }
-    birds[n] = dropout(birds[n])
-  }
+  diedBirds = [];
 }
-function mostFittest(diedBirds){
-  let fittest = null
-  let n = 0
-  for(let i=0; i<diedBirds.length; i++){
-    if(diedBirds[i].score>n){
-      n = i
-      fittest = diedBirds[i]
-    }
+
+function pickOne() {
+  let index = 0;
+  let r = Math.random(1);
+  while (r > 0) {
+    r = r - diedBirds[index].fitness;
+    index++;
   }
-  return {'fittest': fittest, 'number': n}
+  index--;
+  let b = diedBirds[index];
+  let child = new bird(canvas, 20, 'rgba(255, 255, 255, 0.3)',b.brain);
+  child.mutate(0.9);
+  return child;
 }
-function tenFittest(diedBirds, genes){
-  let dBirds = diedBirds
-  let tfittest = []
-  for(let i=0; i<10; i++){
-    let fittest = mostFittest(dBirds)
-    tfittest.push(fittest.fittest)
-    dBirds.splice(fittest.number, 1)
+
+function calculateFitness() {
+  // Normalize all values
+  let sum = 0;
+  for (let bird of diedBirds) {
+    sum += bird.fitness;
   }
-  if(genes){
-    for(let i=0; i<3; i++){
-      if(tfittest[i].score<genes[i].score){
-        tfittest[tfittest.length-(i+1)] = genes[i]
-        console.log('replaced')
-      }
-    }
+  for (let bird of diedBirds) {
+    bird.fitness = bird.fitness / sum;
   }
-  return tfittest
-}
-function dropout(b){
-  let brain = new fnn(b.brain.neurons, b.brain.lr)
-  let n = new bird(canvas, 20, 'rgba(255,0,0,0.4)', brain)
-  return n
 }
